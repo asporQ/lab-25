031abf55b513f5c51a5bfbbb68ea217ea05324e5
asporQ <tikump.aspor@gmail.com>
2023-03-11 15:40:53 +0700

    completed

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
-//Write function count() here.
+int count(int *a ,int b){
+	set<int> myset;
+	int count = 0;
+	for(int i = 0;i<b;i++){
+		myset.insert(a[i]);
+	}
+
+	set<int>::iterator i;
+	for(i = myset.begin();i != myset.end(); i++){
+		count++;
+	}
+	return count;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
@@ -12,3 +25,4 @@ int main(){
 	
 	return 0;
 }
+
